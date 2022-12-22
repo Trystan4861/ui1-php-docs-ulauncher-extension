23fbf014570776d91839c37232956f0fcd0d2d98
Trystan4861 <trystan4861@gmail.com>
2022-12-22 12:35:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Documentación de la Extensión UI1 PHP de Ulauncher
+Extensión para [Ulauncher](https://ulauncher.io/). Búsca en la documentación de PHP en [php.net](https://www.php.net/docs.php).
+
+# Instalación
+1. Instalar [Ulauncher](https://ulauncher.io/#Download). 
+2. Simplemente añadir  `https://github.com/Trystan4861/ui1-php-docs-ulauncher-extension` en la sección de Extensiones de la configuración de Ulauncher.
---
+++ main.py
@@ -0,0 +1,63 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, PreferencesEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
+search_index = {}
+
+
+class PhpDocsExtension(Extension):
+    def __init__(self):
+        super().__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(PreferencesEvent, PreferencesEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+        results_count = 0
+        query_string = event.get_argument()
+
+        for page_index in search_index:
+            if results_count >= extension.preferences['php_max_items']:
+                break
+
+            title = search_index[page_index][0]
+            snippet = search_index[page_index][1]
+
+            if query_string.lower() in title.lower() or query_string.lower() in snippet.lower():
+                results_count += 1
+                items.append(
+                    ExtensionResultItem(
+                        icon='images/icon_ui1.png',
+                        name=title,
+                        description=snippet,
+                        on_enter=OpenUrlAction(
+                            "https://www.php.net/manual/" + extension.preferences['php_lang'] + "/" + page_index
+                        )
+                    )
+                )
+
+        return RenderResultListAction(items)
+
+
+class PreferencesEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        global search_index
+
+        response = requests.get(
+            "https://www.php.net/js/search-index.php?lang=" + event.preferences['php_lang']
+        )
+        search_index = response.json()
+
+if __name__ == '__main__':
+    PhpDocsExtension().run()
